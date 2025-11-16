de6dd6b60e8024695f10dd3db7cd2ef3bab38971
zhaojianhua <1763124707@qq.com>
2025-11-16 21:38:06 +0800

    提交日志 1、开发认证授权的用户中心。

--- auth-center/src/main/java/com/jhzhao/alibaba/AuthApplication.java
+++ auth-center/src/main/java/com/jhzhao/alibaba/AuthApplication.java
@@ -2,11 +2,13 @@ package com.jhzhao.alibaba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * Hello world!
+ * auth-center 的权限认证中心的启动类
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication
 {
