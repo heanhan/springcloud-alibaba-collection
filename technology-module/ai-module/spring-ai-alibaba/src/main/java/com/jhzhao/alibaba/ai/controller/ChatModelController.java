442b9fd6272f604dfd7aaaa90594cd08caf51b86
zhaojianhua <1763124707@qq.com>
2026-03-04 08:02:28 +0800

    提交日志 1、mcp server的开发

--- technology-module/ai-module/spring-ai-alibaba/src/main/java/com/jhzhao/alibaba/ai/controller/ChatModelController.java
+++ technology-module/ai-module/spring-ai-alibaba/src/main/java/com/jhzhao/alibaba/ai/controller/ChatModelController.java
@@ -2,24 +2,16 @@ package com.jhzhao.alibaba.ai.controller;
 
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.content.Media;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
