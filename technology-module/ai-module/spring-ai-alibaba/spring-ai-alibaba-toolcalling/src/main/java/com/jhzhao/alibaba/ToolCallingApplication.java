d40514e3f8900aae375307a345c3cc93124ef1c6
zhaojianhua <1763124707@qq.com>
2026-03-04 18:49:45 +0800

    ai模块的代码  结构化 的demo

---
+++ technology-module/ai-module/spring-ai-alibaba/spring-ai-alibaba-toolcalling/src/main/java/com/jhzhao/alibaba/ToolCallingApplication.java
@@ -0,0 +1,13 @@
+package com.jhzhao.alibaba;
+
+/**
+ * Hello world!
+ *
+ */
+public class ToolCallingApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
